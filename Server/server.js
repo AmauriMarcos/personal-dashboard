dfc0857158b837db52077aa003058bcb6235b30e
AmauriMarcos <amaurisantos.m@gmail.com>
2021-06-18 11:11:36 +0200

    cors little change

--- Server/server.js
+++ Server/server.js
@@ -17,11 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
