32890eca6f40e1945fffb61d76e9a26ce99b8ca0
AmauriMarcos <amaurisantos.m@gmail.com>
2021-06-22 17:08:01 +0200

    resize all images

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -3,31 +3,62 @@ import styles from "./Features.module.css";
 import backgroundLeft from '../../assets/featuresBackgroundLeft.svg';
 import backgroundRight from '../../assets/featuresBackgroundRight.svg'
 import { StyleSharp } from "@material-ui/icons";
+import { useMediaQuery } from "react-responsive";
+import { Image } from "cloudinary-react";
 
 const Features = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 420px)`}); 
+
+
+  let featuresBackgroundLeftImg = null;
+
+  if(isMobile){
+    featuresBackgroundLeftImg = (
+      <Image cloudName="hrfhxbqio" className={styles.backgroundLeft} publicId="newMobilefeaturesBackgroundLeft_c4w690.jpg" ></Image>
+    )
+  }else{
+    featuresBackgroundLeftImg =(
+      <Image cloudName="hrfhxbqio" className={styles.backgroundLeft} publicId="newfeaturesBackgroundLeft_jwelld.jpg" ></Image>
+    )
+  }
+
+  let featuresBackgroundRightImg = null;
+
+  if(isMobile){
+    featuresBackgroundRightImg = (
+      <Image cloudName="hrfhxbqio" className={styles.backgroundRight}publicId="newMobileBackgroundRight_s0jshv.jpg"></Image>
+    )
+  }else{
+    featuresBackgroundRightImg =(
+      <Image cloudName="hrfhxbqio" className={styles.backgroundRight}publicId="newBackgroundRight_ggzxti.jpg" ></Image>
+    )
+  }
+
   return (
     <div className={styles.Features}>
       <div className={styles.left}>
         <div className={styles.boxImage}>
-          <img  className={styles.backgroundLeft} src={ backgroundLeft}/>  
+           {featuresBackgroundLeftImg}
         </div>
 
         <div className={styles.content}>
-          <h2>The best solution<br></br> for your personal<br></br> finances</h2>
+          <h2>The best solution for your personal finances</h2>
           <p>
-            Set goals and use charts to visualize the data tha t matters to you
+            Set goals and use charts to visualize the data tha t matters to you. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
+          <button>Go to dashboard</button>
         </div>
       </div>
       <div className={styles.right}>
         <div className={styles.content}>
-          <h2>The best solution<br></br> for your personal<br></br> finances</h2>
+          <h2>The best solution for your personal finances</h2>
           <p>
-            Set goals and use charts to visualize the data tha t matters to you
+            Set goals and use charts to visualize the data tha t matters to you. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
+          <button>Get started</button>
         </div>
         <div className={styles.boxImage}>
-          <img  className={styles.backgroundRight} src={ backgroundRight}/>
+           {featuresBackgroundRightImg}
         </div>
       </div>
     </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,35 @@
 import React from "react";
 import styles from "./Product.module.css";
 import iPhonePic from "../../assets/iPhonePic.svg";
-
+import { useMediaQuery } from "react-responsive";
+import { Image } from "cloudinary-react";
+/* publicId="iPhonePic_e1ee3l.jpg" */
 const Product = () => {
+ const isMobile = useMediaQuery({ query: `(max-width: 420px)`}); 
+  let productImg = null;
+
+  if(isMobile){
+    productImg = (
+      <Image cloudName="hrfhxbqio" publicId="iPhonePicMobile_wk4lps.jpg"  ></Image>
+    )
+  }else{
+    productImg =(
+      <Image cloudName="hrfhxbqio" publicId="iPhonePic_e1ee3l.jpg" ></Image>
+    )
+  }
+
   return (
     <div className={styles.Product}>
       <div className={styles.content}>
-        <h2>The best way for managing your<br></br> money</h2>
+        <h2>The best way for managing your money</h2>
         <p>
           It is easy to keep track of your receipts and payments, income and
-          outcome in one financial dashboard.{" "}
+          outcome in one financial dashboard. Set goals and use charts to visualize the data tha t matters to you. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
         <button>Try for free</button>
       </div>
       <div className={styles.image}>
-        <img src={iPhonePic} alt="iPhone" />
+        {productImg}
       </div>
     </div>
   );
